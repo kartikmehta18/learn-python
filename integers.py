bbb43c58cb8508baadd4419a7858bc78bd20cd9c
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-01-10 20:38:04 +0530

    intigers

---
+++ integers.py
@@ -0,0 +1,53 @@
+num =3.14
+
+print (type(num))
+#division 1.5
+print(3 / 2)
+#flor division op= 1
+print(3// 2)
+#any square
+print(3**2)
+# mod reminder
+print(3%2)
+print(2%2)
+
+# order of operations first exicute this (2+1) then *
+print(3* (2+1))
+
+# increment
+i=1
+i= i+1
+print(i)
+
+i+= 1
+i*= 10
+print(i)
+
+# round off
+print(round(3.75))
+# roundoff 1st digit FTER THE desimal
+print(round(3.75, 1))
+
+# comparision operators
+
+
+num1 =3
+num2 =  2
+
+
+print(num1 == num2)  # gives bool op
+
+print(num1 != num2)
+
+print(num1 >= num2)
+print(num1 <= num2)
+
+# string value to num
+n1= "100"
+n2 ="200"
+print(n1 + n2)
+#casting
+n1 =  int(n1)
+n2 = int(n2)
+
+print(n1 + n2)
