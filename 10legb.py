5b8cbe6924e9a77d72c65adc95c316ed7b4f13aa
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2025-01-30 19:54:06 +0530

    global, local

--- 10legb.py
+++ 10legb.py
@@ -1,2 +1,14 @@
 #LEGE  lOCAL, ENCLOSING GLOBAL, BUILT-IN
 
+# x = 'gloal x'  # global define
+
+def test():
+    global x # working as global variable
+    x = 'local x'
+    y='local y' # local define cannot acces out side of function
+    print(y)
+    print(x)
+test()
+print(x)
+
+    
